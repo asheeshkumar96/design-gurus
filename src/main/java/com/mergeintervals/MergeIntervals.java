2669871836e1c87b12c3962122dbacdc56d560b7
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-01-03 00:03:33 +0530

    tried merge intervals

--- src/main/java/com/mergeintervals/MergeIntervals.java
+++ src/main/java/com/mergeintervals/MergeIntervals.java
@@ -3,6 +3,7 @@ package com.mergeintervals;
 import com.mergeintervals.MergeIntervals;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 class Interval {
   int start;
@@ -12,25 +13,71 @@ class Interval {
     this.start = start;
     this.end = end;
   }
+
+  public String toString() {
+    return "[" + start + ", " + end + "] ";
+  }
 }
 
 public class MergeIntervals {
 
   public static List<Interval> merge(List<Interval> intervals) {
+    // If passed intervals size is one or less than one , return
+    if(intervals.size() <= 1) {
+      return intervals;
+    }
+    // Sort the intervals
+    Collections.sort(intervals, (interval1, interval2) -> Integer.compare(interval1.start, interval2.start));
+    System.out.println("Intervals after sorting:" + intervals);
     List<Interval> mergedIntervals = new ArrayList<Interval>();
+    Interval currentInterval = intervals.get(0);
+    mergedIntervals.add(currentInterval); 
+    
+    for(Interval interval: intervals) {
+      int currentStart = currentInterval.start;
+      int currentEnd = currentInterval.end;
+      int nextStart = interval.start;
+      int nextEnd = interval.end;
+
+      if(currentEnd >= nextStart) {
+        currentInterval.end = Math.max(currentEnd, nextEnd);
+      } else {
+        currentInterval = interval;
+        mergedIntervals.add(currentInterval);
+      }
+    }
 
     return mergedIntervals;
   }
 
   public static void main(String[] args) {
-
     List<Interval> input = new ArrayList<Interval>();
     input.add(new Interval(1, 4));
     input.add(new Interval(2, 5));
     input.add(new Interval(7, 9));
     System.out.print("Merge Intervals: ");
     for (Interval interval : MergeIntervals.merge(input)) {
-      System.out.print(" [ " + interval.start + ", " + interval.end + " ] ");
+      System.out.print("[ " + interval.start + ", " + interval.end + " ] ");
+    }
+    System.out.println();
+
+    input = new ArrayList<Interval>();
+    input.add(new Interval(6, 7));
+    input.add(new Interval(2, 4));
+    input.add(new Interval(5, 9));
+    System.out.print("Merge Intervals: ");
+    for (Interval interval : MergeIntervals.merge(input)) {
+      System.out.print("[ " + interval.start + ", " + interval.end + " ] ");
+    }
+    System.out.println();
+
+    input = new ArrayList<Interval>();
+    input.add(new Interval(1, 4));
+    input.add(new Interval(2, 6));
+    input.add(new Interval(3, 5));
+    System.out.print("Merge Intervals: ");
+    for (Interval interval : MergeIntervals.merge(input)) {
+      System.out.print("[ " + interval.start + ", " + interval.end + " ] ");
     }
     System.out.println();
   }
