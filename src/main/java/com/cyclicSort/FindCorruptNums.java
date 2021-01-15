6d7742b42ded9eceb71386ead30fbc5d90cc5fba
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-01-15 07:08:47 +0530

    Cyclic sort is very very hard

--- src/main/java/com/cyclicSort/FindCorruptNums.java
+++ src/main/java/com/cyclicSort/FindCorruptNums.java
@@ -4,31 +4,45 @@ import java.util.Arrays;
 
 public class FindCorruptNums {
 
-  public static int[] findNumbers(int[] nums) {
-    int duplicate = -1;
-    int missing = -1;
-    for(int i = 0; i < nums.length; i++) {
-      if(nums[i] == nums[nums[i]-1]) {
-          duplicate = nums[i];
-          missing = i + 1;
-          break;
-        } else {
-          if(nums[i] != i + 1)  {
-            swap(nums, i, nums[i]-1);
-          }
-        }
-    }
-    return new int[]{duplicate, missing};
-  }
-  
-  private  static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+	public static int[] findNumbers(int[] nums) {
+		int duplicate = -1;
+		int missing = -1;
+		int i = 0;
+		while (i < nums.length) {
+			//System.out.println("index = " + i + " the number should be = " + (i + 1) + " but it is actually = " + nums[i]);
+			if (nums[i] != i + 1) {
+				if (nums[nums[i] - 1] == nums[i]) {
+					duplicate = nums[i];
+					//System.out.println("Found the duplicate...");
+					i++;
+				} else {
+					//System.out.println("BeforeSwapping: " + Arrays.toString(nums));
+					swap(nums, i, nums[i] - 1);
+					//System.out.println("After swapping: " + Arrays.toString(nums));
+				}
+			} else {
+				i++;
+			}
+		}
 
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(findNumbers(new int[] {3, 1, 2, 5, 2})));
-    System.out.println(Arrays.toString(findNumbers(new int[] {3, 1, 2, 3, 6, 4})));
-  }
+		missing = nums.length;
+		for (i = 0; i < nums.length; i++) {
+			// System.out.println("current value: " + nums[i]);
+			if (nums[i] != i + 1) {
+				missing = i + 1;
+			}
+		}
+		return new int[] {duplicate, missing};
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(FindCorruptNums.findNumbers(new int[] { 3, 1, 2, 5, 2 })));
+		System.out.println(Arrays.toString(FindCorruptNums.findNumbers(new int[] { 3, 1, 2, 3, 6, 4 })));
+	}
 }
