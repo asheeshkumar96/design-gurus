15f7e90f1e6a776e0e25ddc0913e414185d003e4
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-02-14 09:53:17 +0530

    letter permutations, how is it bfs?

--- src/main/java/com/subsets/LetterCaseStringPermutations.java
+++ src/main/java/com/subsets/LetterCaseStringPermutations.java
@@ -8,7 +8,35 @@ public class LetterCaseStringPermutations {
 	public static List<String> findLetterCaseStringPermutations(String str) {
 		List<String> permutations = new ArrayList<>();
 		// TODO: Write your own code
+		permutations.add(str);
+		letterCasePermutations(str, permutations, 0);
 		return permutations;
+	}
+	
+	private static void letterCasePermutations(String str, List<String> permutations, int letterIndex) {
+		// base case
+		if(letterIndex == str.length()) return;
+		
+		char charAtLetterIndex = str.charAt(letterIndex);
+		if(Character.isLowerCase(charAtLetterIndex)) {
+			int length = permutations.size();
+			for(int i = 0; i < length ; i++) {
+				StringBuilder perm = new StringBuilder(permutations.get(i));
+				perm.setCharAt(letterIndex, Character.toUpperCase(charAtLetterIndex));
+				permutations.add(perm.toString());
+			}
+			
+		} else if(Character.isUpperCase(charAtLetterIndex)) {
+			int length = permutations.size();
+			for(int i = 0; i < length ; i++) {
+				StringBuilder perm = new StringBuilder(permutations.get(i));
+				perm.setCharAt(letterIndex, Character.toLowerCase(charAtLetterIndex));
+				permutations.add(perm.toString());
+			}
+		}
+		
+		letterCasePermutations(str, permutations, letterIndex+1);
+		
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/subsets/LetterCaseStringPermutationsOfficial.java
@@ -0,0 +1,42 @@
+package com.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// official
+public class LetterCaseStringPermutationsOfficial {
+	
+	// official, time and space complexity both are O(N*2^n)
+	public static List<String> findLetterCaseStringPermutations(String str) {
+		List<String> permutations = new ArrayList<>();
+		if(str == null)
+			return permutations;
+		
+		permutations.add(str);
+		// process every character of the string one by one
+		for(int i = 0; i < str.length(); i++) {
+			if (Character.isLetter(str.charAt(i))) { // only process characters, skip digits
+				// we will take all existing permutations and change the letter case appropriately
+				int n = permutations.size();
+				for(int j = 0; j < n; j++) {
+					char[] chs = permutations.get(j).toCharArray();
+					// if the character is in upper case change it to lower case or vice versa
+					if(Character.isUpperCase(chs[i]))
+						chs[i] = Character.toLowerCase(chs[i]);
+					else
+						chs[i] = Character.toUpperCase(chs[i]);
+					
+					permutations.add(String.valueOf(chs));
+				}
+			}
+		}
+		return permutations;
+	}
+	
+	public static void main(String[] args) {
+		List<String> result = LetterCaseStringPermutationsOfficial.findLetterCaseStringPermutations("ad52");
+		System.out.println(" String permutations are: " + result);
+		result = LetterCaseStringPermutationsOfficial.findLetterCaseStringPermutations("ab7c");
+		System.out.println(" String permutations are: " + result);
+	}
+}
