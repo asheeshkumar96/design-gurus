a5dec420b00ec04c7c70e88be22d540dd40b1824
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-02-16 23:51:34 +0530

    complement, horizontal flip and invert

---
+++ src/main/java/com/bitwiseXor/FlipAndInvert.java
@@ -0,0 +1,35 @@
+package com.bitwiseXor;
+
+// also, official
+public class FlipAndInvert {
+	
+	public static int[][] flipAndInvertImage(int[][] arr) {
+		int C = arr[0].length; 
+		// swap ith column value from left to ith column from right of a row, to invert XOR is performed with 1
+		for(int[] row : arr) {
+			for(int i = 0; i < (C + 1) / 2 ; i++) {
+				int tmp = row[i]^1;
+				row[i] = row[C - 1 - i] ^ 1;
+				row[C - 1 - i] = tmp;
+			}
+		}
+		return arr;
+	}
+	
+	public static void print(int[][] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		int[][] arr = {{1, 0, 1}, {1, 1, 1}, {0, 1, 1}};
+		print(flipAndInvertImage(arr));
+		int[][] arr2 = {{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}};
+		print(flipAndInvertImage(arr2));
+	}
+
+}
