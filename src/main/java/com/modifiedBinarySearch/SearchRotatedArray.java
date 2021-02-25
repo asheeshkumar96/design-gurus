46382035fee9bafbf42c428bd61dd0825621bcec
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-02-25 16:20:53 +0530

    applying binary search in sorted rotated array

--- src/main/java/com/modifiedBinarySearch/SearchRotatedArray.java
+++ src/main/java/com/modifiedBinarySearch/SearchRotatedArray.java
@@ -2,7 +2,31 @@ package com.modifiedBinarySearch;
 
 public class SearchRotatedArray {
 	
+	// official, but how, a bit tough
 	public static int search(int[] arr, int key) {
+		int start = 0, end = arr.length - 1;
+		while(start <= end) {
+			int mid = start + (end - start) / 2;
+			if(arr[mid] == key) 
+				return mid;
+			
+			if(arr[start] <= arr[mid]) { // left side is sorted in ascending order
+				if(key >= arr[start] && key < arr[mid]) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			
+			} else { // right side is sorted in ascending order
+				if( key > arr[mid] && key <= arr[end]) {
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			}
+		}
+		
+		// we are not able to find the element in the given array
 		return -1;
 	}
 
