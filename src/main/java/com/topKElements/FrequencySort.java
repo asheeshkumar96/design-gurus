ffece2e3eb5cff709012fe6c5678c57cfe90215c
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-03-17 00:30:25 +0530

    K closest very good problem, also its two pointer approach

---
+++ src/main/java/com/topKElements/FrequencySort.java
@@ -0,0 +1,40 @@
+package com.topKElements;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+
+public class FrequencySort {
+	
+	// also official
+	public static String sortCharacterByFrequency(String str) {
+		// find the frequency of each character
+		Map<Character, Integer> characterFrequencyMap = new HashMap<Character, Integer>();
+		for(char chr : str.toCharArray()) {
+			characterFrequencyMap.put(chr, characterFrequencyMap.getOrDefault(chr, 0) + 1);
+		}
+		
+		PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<Map.Entry<Character, Integer>>(
+				(e1, e2) -> e2.getValue() - e1.getValue());
+		// add all characters to the max heap
+		maxHeap.addAll(characterFrequencyMap.entrySet());
+		
+		// build a string, appending the most occuring characters first
+		StringBuilder sortedString = new StringBuilder(str.length());
+		while(!maxHeap.isEmpty()) {
+			Entry<Character, Integer> entry = maxHeap.poll();
+			for(int i = 0; i < entry.getValue(); i++)
+				sortedString.append(entry.getKey());
+		}
+		return sortedString.toString();
+	}
+	
+	public static void main(String[] args) {
+		String result = FrequencySort.sortCharacterByFrequency("Programming");
+		System.out.println("Here is the given string after sorting characters by frequency: " + result);
+		
+		result = FrequencySort.sortCharacterByFrequency("abcbab");
+		System.out.println("Here is the given string after sorting characters by frequency: " + result);
+	}
+}
