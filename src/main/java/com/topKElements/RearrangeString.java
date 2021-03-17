4de9caa8dc632de9e033fae3a6bcded35d82742c
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-03-17 17:41:45 +0530

    re-arranged string s.t. no consecutive chars are same, very hard

--- src/main/java/com/topKElements/RearrangeString.java
+++ src/main/java/com/topKElements/RearrangeString.java
@@ -1,16 +1,71 @@
 package com.topKElements;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+
 public class RearrangeString {
 	
+	// official
 	public static String rearrangeString(String str) {
-		StringBuilder sb = new StringBuilder();
-		return sb.toString();
+		Map<Character, Integer> charFrequencyMap = new HashMap<>();
+		for(char chr : str.toCharArray())
+			charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+		
+		PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<Map.Entry<Character, Integer>>(
+				(e1, e2) -> e2.getValue() - e1.getValue());
+
+		// add all characters to the max heap
+		maxHeap.addAll(charFrequencyMap.entrySet());
+		
+		Map.Entry<Character, Integer> previousEntry = null;
+		StringBuilder resultString = new StringBuilder();
+		while(!maxHeap.isEmpty()) { // very tricky
+			Entry<Character, Integer> currentEntry = maxHeap.poll();
+			// add the previous entry back in the heap if its frequency is greater than zero
+			if(previousEntry != null && previousEntry.getValue() > 0)
+				maxHeap.offer(previousEntry);
+			// append the current character to the result string and decrement its count
+			resultString.append(currentEntry.getKey());
+			currentEntry.setValue(currentEntry.getValue() - 1);
+			previousEntry = currentEntry;
+		}
+		
+		return resultString.length() == str.length() ? resultString.toString() : "";
+	}
+	
+	/**
+	 Returns whether the string can be arranged in such a way that no two adjacent characters are some
+	 * @param str the string to check for possible arrangement
+	 * @return true iff the string can be arranged such that no two adjacent chars are same, false otherwise
+	 */
+	public static boolean canBeArranged(String str) {
+		int n = str.length(); 
+		Map<Character, Integer> charFrequencyMap = new HashMap<>();
+		int max = 0;
+		for(char chr: str.toCharArray()) {
+			charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+			if(charFrequencyMap.get(chr) > max)
+				max = charFrequencyMap.get(chr);
+		}
+		
+		if(max <= n - max + 1)
+			return true;
+		else
+			return false;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Rearranged string: " + RearrangeString.rearrangeString("aappp"));
 		System.out.println("Rearranged string: " + RearrangeString.rearrangeString("Programming"));
 		System.out.println("Rearranged string: " + RearrangeString.rearrangeString("aapa"));
+		
+		/*
+		System.out.println("Rearranged string: " + RearrangeString.canBeArranged("aappp"));
+		System.out.println("Rearranged string: " + RearrangeString.canBeArranged("Programming"));
+		System.out.println("Rearranged string: " + RearrangeString.canBeArranged("aapa"));
+		*/
 	}
 
 }
--- src/main/java/com/topologicalSort/TopologicalSort.java
+++ src/main/java/com/topologicalSort/TopologicalSort.java
@@ -7,19 +7,21 @@ public class TopologicalSort {
 	
 	public static List<Integer> sort(int vertices, int[][] edges) {
 		List<Integer> sortedOrder = new ArrayList<>();
-		// TODO: Write your own code here
+		
 		return sortedOrder;
 	}
 	
 	public static void main(String[] args) {
-		// TODO: to fill the values in two dim array
-		List<Integer> result = TopologicalSort.sort(4, new int[][] { });
+		List<Integer> result = TopologicalSort.sort(4,
+				new int[][] { new int[] { 3, 2 }, new int[] { 3, 0 }, new int[] { 2, 0 }, new int[] { 2, 1 } });
 		System.out.println(result);
 		
-		result = TopologicalSort.sort(5, new int[][] { });;
+		result = TopologicalSort.sort(5,
+				new int[][] { new int[] { 4, 2 }, new int[] { 4, 3 }, new int[] { 2, 0 }, new int[] { 2, 1 }, new int[] {3, 1} });
 		System.out.println(result);
 		
-		result = TopologicalSort.sort(7, new int[][] { });
+		result = TopologicalSort.sort(7, new int[][] { new int[] { 6, 4 }, new int[] { 6, 2 }, new int[] { 5, 3 },
+				new int[] { 5, 4 }, new int[] { 3, 0 }, new int[] { 3, 1 }, new int[] { 3, 2 }, new int[] { 4, 1 } });
 		System.out.println(result);
 	}
 
