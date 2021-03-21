5418a5983fe3d7acf794787915fad79da8296d2a
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-03-21 23:20:42 +0530

    sliding window and two pointer is yet left

---
+++ src/main/java/com/twoPointers/PairWithTargetSum.java
@@ -0,0 +1,12 @@
+package com.twoPointers;
+
+public class PairWithTargetSum {
+	
+	public static int[] search(int[] arr, int targetSum) {
+		return new int[] {-1, -1};
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/main/java/slidingWindow/MaxSumSubArrayOfSizeK.java
@@ -0,0 +1,9 @@
+package slidingWindow;
+
+public class MaxSumSubArrayOfSizeK {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
