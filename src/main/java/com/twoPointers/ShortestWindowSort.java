e4da104639356afdac1eb2b9cf6e198ff5a474c3
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-03-25 22:45:31 +0530

    shortest window sort

--- src/main/java/com/twoPointers/ShortestWindowSort.java
+++ src/main/java/com/twoPointers/ShortestWindowSort.java
@@ -2,12 +2,50 @@ package com.twoPointers;
 
 public class ShortestWindowSort {
 	
+	// official, O(N) time complexity, O(1) space complexity
 	public static int sort(int[] arr) {
-		return -1;
+		int low = 0, high = arr.length - 1;
+		// find the first number out of sorting order from beginning
+		while(low < arr.length - 1 && arr[low] <= arr[low + 1])
+			low++;
+		
+		if(low == arr.length - 1) // if the array is sorted
+			return 0;
+		
+		// find the first number out of sorint orer from the end
+		while(high > 0 && arr[high] >= arr[high - 1])
+			high--;
+		
+		// find the maximum and minimum of the subarray
+		int subarrayMax = Integer.MIN_VALUE, subarrayMin = Integer.MAX_VALUE;
+		for(int k = low; k <= high; k++) {
+			subarrayMax = Math.max(subarrayMax, arr[k]);
+			subarrayMin = Math.min(subarrayMin, arr[k]);
+			/*
+			if(arr[k] < subarrayMin)
+				subarrayMin = arr[k];
+			if(arr[k] > subarrayMax)
+				subarrayMax = arr[k];
+			*/
+		}
+		
+		// extend the subarray to include any number which is bigger than the minimum of the subarray
+		while (low > 0 && arr[low - 1] > subarrayMin)
+			low--;
+		
+		// extend the subarray to include any number which is smaller than the maximum of the subarray
+		while(high < arr.length - 1 && arr[high + 1] < subarrayMax)
+			high++;
+		
+		System.out.println("hight Index = " + high + ", low index=" + low);
+		
+		return high - low + 1;
 	}
 
 	public static void main(String[] args) {
-		
+		System.out.println(ShortestWindowSort.sort(new int[] { 1, 2, 5, 3, 7, 10, 9, 12 }));
+		System.out.println(ShortestWindowSort.sort(new int[] { 1, 3, 2, 0, -1, 7, 10 }));
+		System.out.println(ShortestWindowSort.sort(new int[] { 1, 2, 3 }));
+		System.out.println(ShortestWindowSort.sort(new int[] { 3, 2, 1 }));
 	}
-
 }
